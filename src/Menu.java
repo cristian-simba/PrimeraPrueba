ce5d72faf50e63ddbdfb3198bfcab6ab5570ec8f
cristian-simba <davidsimba200@gmail.com>
2023-06-02 12:17:00 -0500

    Prueba

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Menu {
+public class Menu {
+    public static void main(String[] args) {
+        //Creación de las instancias de 5 diferentes videojuegos
+        Videojuegos game1 = new Videojuegos("Super Mario Bros","Nintendo", "Plataformas", 2002, 1500000);
+        Videojuegos game2 = new Videojuegos("The legend of Zelda", "Nintendo","Aventura",2023, 2000000);
+        Videojuegos game3 = new Videojuegos("Call of Duty","Activision","Acción y Shotter", 2019, 1589032);
+        Videojuegos game4 = new Videojuegos("Halo 5", "Microsoft", "Shotter FPS", 2022, 15978341);
+        Videojuegos game5 = new Videojuegos("Fornite", "Epic Games", "Shotter", 2018, 159753152);
+
+        // Impresión de los datos que tienen los videojuegos
+        System.out.println("Primer Videojuego");
+        game1.imprimirDatos();
+        System.out.println("Segundo Videojuego");
+        game2.imprimirDatos();
+        System.out.println("Tercer Videojuego");
+        game3.imprimirDatos();
+        System.out.println("Cuarto Videojuego");
+        game4.imprimirDatos();
+        System.out.println("Quinto Videojuego");
+        game5.imprimirDatos();
+
+    }
 }
--- src/Videojuegos.java
+++ src/Videojuegos.java
@@ -1,2 +1,66 @@
-package PACKAGE_NAME;public class Videojuegos {
+public class Videojuegos {
+    //Creamos los atributos que van a tener los futuros objetos
+    String nombre, empresaDesarrollo, genero;
+    int fechaLanzamiento, ventas;
+
+    //Creacion del constructor para todas las variables
+    public Videojuegos(String nombre, String empresaDesarrollo, String genero, int fechaLanzamiento, int ventas) {
+        this.nombre = nombre;
+        this.empresaDesarrollo = empresaDesarrollo;
+        this.genero = genero;
+        this.fechaLanzamiento = fechaLanzamiento;
+        this.ventas = ventas;
+    }
+
+    //Creacion de getters y setters para todas las variables
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmpresaDesarrollo() {
+        return empresaDesarrollo;
+    }
+
+    public void setEmpresaDesarrollo(String empresaDesarrollo) {
+        this.empresaDesarrollo = empresaDesarrollo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getFechaLanzamiento() {
+        return fechaLanzamiento;
+    }
+
+    public void setFechaLanzamiento(int fechaLanzamiento) {
+        this.fechaLanzamiento = fechaLanzamiento;
+    }
+
+    public int getVentas() {
+        return ventas;
+    }
+
+    public void setVentas(int ventas) {
+        this.ventas = ventas;
+    }
+
+    // Agregamos un método para imprimir los datos
+    public void imprimirDatos(){
+        System.out.println("El nombre del juego es: "+this.nombre);
+        System.out.println("La empresa desarrolladora es: "+this.empresaDesarrollo);
+        System.out.println("El género es: "+this.genero);
+        System.out.println("La fecha de lanzamiento fue el: "+this.fechaLanzamiento);
+        System.out.println("La recaudacion en ventas es de: "+this.ventas+" dólares");
+        System.out.println(" ");
+    }
 }
